d55e9e333d4239de3d4073632b3b1e393c51b523
prassuja <prassuja@isu.edu>
2022-01-09 14:57:52 -0700

    minor changes

--- read.py
+++ read.py
@@ -15,7 +15,7 @@ def get_json_reader(base_dir, table_name, chunksize=1000):
     return pd.read_json(fp, lines=True, chunksize=chunksize)
 
 if __name__ == '__main__':
-    base_dir = os.environ.get('base_dir') # would be same as above and set as environment variables
+    base_dir = os.environ.get('base_dir') # would be same as above and set as environment variables under 'Run' - environmnet variables
     table_name = os.environ.get('table_name') # same as base_dir
     json_reader = get_json_reader(base_dir, table_name)
     for idx, df in enumerate(json_reader):
